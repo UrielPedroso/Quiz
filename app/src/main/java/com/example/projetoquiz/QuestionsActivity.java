2830d66a7cc82965c6f7b784796b7527c472b6f2
urel <urielpedroso91@gmail.com>
2023-12-18 22:20:47 -0300

    first commit

---
+++ app/src/main/java/com/example/projetoquiz/QuestionsActivity.java
@@ -0,0 +1,134 @@
+package com.example.projetoquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuestionsActivity extends AppCompatActivity {
+    TextView tv;
+    Button submitbutton, quitbutton;
+    RadioGroup radio_g;
+    RadioButton rb1,rb2,rb3,rb4;
+
+    String questions[] = {
+            "Qual método pode ser definido apenas uma vez em um programa?",
+            "Qual destes não é um operador bit a bit?",
+            "Qual palavra-chave é usada pelo método para se referir ao objeto que o invocou?",
+            "Qual dessas palavras-chave é usada para definir interfaces em Java?",
+            "Qual desses especificadores de acesso pode ser usado para uma interface?",
+            "Qual das seguintes é a maneira correta de importar um pacote inteiro 'pkg'?",
+            "Qual é o tipo de retorno dos Construtores?",
+            "Qual dos seguintes pacotes armazena todas as classes java padrão?",
+            "Qual desses métodos da classe String é usado para comparar dois objetos String quanto à igualdade?",
+            "Uma expressão envolvendo byte, int e números literais é promovida para qual desses?"
+    };
+    String answers[] = {"main method","<=","this","interface","public","import pkg.*","Nenhuma das alternativas","java","equals()","int"};
+    String opt[] = {
+            "finalize method","main method","static method","private method",
+            "&","&=","|=","<=",
+            "import","this","catch","abstract",
+            "Interface","interface","intf","Intf",
+            "public","protected","private","Todos os mencionados",
+            "Import pkg.","import pkg.*","Import pkg.*","import pkg.",
+            "int","float","void","Nenhuma das alternativas",
+            "lang","java","util","java.packages",
+            "equals()","Equals()","isequal()","Isequal()",
+            "int","long","byte","float"
+    };
+    int flag=0;
+    public static int marks=0,correct=0,wrong=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        final TextView score = (TextView)findViewById(R.id.textView4);
+        TextView textView=(TextView)findViewById(R.id.DispName);
+        Intent intent = getIntent();
+        String name= intent.getStringExtra("myname");
+
+        if (name.trim().equals(""))
+            textView.setText("Ola Jogador");
+        else
+            textView.setText("Ola " + name);
+
+        submitbutton=(Button)findViewById(R.id.button3);
+        quitbutton=(Button)findViewById(R.id.buttonquit);
+        tv=(TextView) findViewById(R.id.tvque);
+
+        radio_g=(RadioGroup)findViewById(R.id.answersgrp);
+        rb1=(RadioButton)findViewById(R.id.radioButton);
+        rb2=(RadioButton)findViewById(R.id.radioButton2);
+        rb3=(RadioButton)findViewById(R.id.radioButton3);
+        rb4=(RadioButton)findViewById(R.id.radioButton4);
+        tv.setText(questions[flag]);
+        rb1.setText(opt[0]);
+        rb2.setText(opt[1]);
+        rb3.setText(opt[2]);
+        rb4.setText(opt[3]);
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //int color = mBackgroundColor.getColor();
+                //mLayout.setBackgroundColor(color);
+
+                if(radio_g.getCheckedRadioButtonId()==-1)
+                {
+                    Toast.makeText(getApplicationContext(), "Selecione uma opcão\n", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RadioButton uans = (RadioButton) findViewById(radio_g.getCheckedRadioButtonId());
+                String ansText = uans.getText().toString();
+//                Toast.makeText(getApplicationContext(), ansText, Toast.LENGTH_SHORT).show();
+                if(ansText.equals(answers[flag])) {
+                    correct++;
+                    Toast.makeText(getApplicationContext(), "Correto", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    wrong++;
+                    Toast.makeText(getApplicationContext(), "Incorreto", Toast.LENGTH_SHORT).show();
+                }
+
+                flag++;
+
+                if (score != null)
+                    score.setText(""+correct);
+
+                if(flag<questions.length)
+                {
+                    tv.setText(questions[flag]);
+                    rb1.setText(opt[flag*4]);
+                    rb2.setText(opt[flag*4 +1]);
+                    rb3.setText(opt[flag*4 +2]);
+                    rb4.setText(opt[flag*4 +3]);
+                }
+                else
+                {
+                    marks=correct;
+                    Intent in = new Intent(getApplicationContext(),ResultActivity.class);
+                    startActivity(in);
+                }
+                radio_g.clearCheck();
+            }
+        });
+
+        quitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
